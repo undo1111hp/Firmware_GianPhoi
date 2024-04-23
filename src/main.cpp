36985f514674953675a3b9b3f2087893628f7f78
undo1111hp <sontunghero36@gmail.com>
2024-04-23 19:37:35 +0700

    Add button and code for PWM

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,43 @@
 #include <Arduino.h>
 
-int motor_p = 4;
-int motor_n = 5;
-int button = 14;
+int motor_p = 4; // D2
+int motor_n = 5; // D1
+int button_left = 14; // D5
+int button_right = 12; // D6
+int count = 1;
 
 void setup(){	
     pinMode(motor_p, OUTPUT);
     pinMode(motor_n, OUTPUT);
-    pinMode(button, INPUT_PULLUP);
+    pinMode(button_left, INPUT_PULLUP);
+    pinMode(button_right, INPUT_PULLUP);
 }
 void loop(){
-    if(digitalRead(button) == HIGH){
-    digitalWrite(motor_p, LOW);
-    digitalWrite(motor_n, HIGH); 
-    delay(1000);
+    int speed = (count*6);
+    constrain(speed, 1, 191);
+    if(digitalRead(button_left) == HIGH && digitalRead(button_right) == LOW){
+    analogWrite(motor_p, LOW);
+    analogWrite(motor_n, speed); 
+    count++;
+    delay(100);
     }
-    if(digitalRead(button) == LOW){
-    digitalWrite(motor_p, LOW);
-    digitalWrite(motor_n, LOW); 
-    delay(1000);
+    if(digitalRead(button_left) == LOW && digitalRead(button_right) == HIGH){
+    analogWrite(motor_p, LOW);
+    analogWrite(motor_n, speed); 
+    count++;
+    delay(100);
+    }
+    if(digitalRead(button_left) == HIGH && digitalRead(button_right) == HIGH){
+    analogWrite(motor_p, LOW);
+    analogWrite(motor_n, LOW); 
+    count = 1;
+    delay(100);
+    }
+    if(digitalRead(button_left) == LOW && digitalRead(button_right) == LOW){
+    analogWrite(motor_p, LOW);
+    analogWrite(motor_n, LOW); 
+    count = 1;
+    delay(100);
     }
 }
 
